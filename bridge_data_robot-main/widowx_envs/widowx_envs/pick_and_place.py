46fe4f5d15599e976eae50372a76b1ebcfd06543
wyoung1810 <ukjoma@gmail.com>
2024-12-07 17:12:39 -0500

    updated pick and place to move A4 pawn

--- bridge_data_robot-main/widowx_envs/widowx_envs/pick_and_place.py
+++ bridge_data_robot-main/widowx_envs/widowx_envs/pick_and_place.py
@@ -6,41 +6,7 @@ import cv2
 import time
 from widowx_envs.widowx_env_service import WidowXClient, WidowXConfigs, WidowXStatus
 
-def show_video(client, full_image=True):
-    """
-    This shows the video from the camera for a given duration.
-    Full image is the image before resized to default 256x256.
-    """
-    res = client.get_observation()
-    if res is None:
-        print("No observation available... waiting")
-        return
-
-    if full_image:
-        img = res["full_image"]
-        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-    else:
-        img = res["image"]
-        img = (img.reshape(3, 256, 256).transpose(1, 2, 0) * 255).astype(np.uint8)
-    cv2.imshow("Robot Camera", img)
-    cv2.waitKey(20)  # 20 ms
-
-print_yellow = lambda x: print("\033[93m {}\033[00m" .format(x))
-
-
-def print_help():
-    print_yellow("  Teleop Controls:")
-    print_yellow("    w, s : move forward/backward")
-    print_yellow("    a, d : move left/right")
-    print_yellow("    z, c : move up/down")
-    print_yellow("    i, k:  rotate yaw")
-    print_yellow("    j, l:  rotate pitch")
-    print_yellow("    n  m:  rotate roll")
-    print_yellow("    space: toggle gripper")
-    print_yellow("    r: reset robot")
-    print_yellow("    q: quit")
-
-def pick_and_place(xy_initial, xy_final, height, clearance_height, client, narrow=False):
+def pick_and_place(xy_initial, xy_final, height, clearance_height, client, gripper_width=1):
     '''
     Pick up an object at a certain position  and place it at a different position.
 
@@ -54,11 +20,9 @@ def pick_and_place(xy_initial, xy_final, height, clearance_height, client, narro
     x_initial = xy_initial[0]
     y_initial = xy_initial[1]
     x_final = xy_final[0]
-    y_final = xy_initial[1]
+    y_final = xy_final[1]
 
-    if narrow:
-        client.move_gripper(0.5)
-        time.sleep(2)
+    client.move_gripper(gripper_width)
 
     client.move(np.array([0.3, 0, 0.15, 0, 1.57, 0])) # Move home
     time.sleep(4)
@@ -74,7 +38,7 @@ def pick_and_place(xy_initial, xy_final, height, clearance_height, client, narro
     time.sleep(4)
     client.move(np.array([x_final, y_final, height, 0, 1.57, 0]))
     time.sleep(4)
-    client.move_gripper(1.0 - 0.5 * narrow) # moves to narrow if narrow, otherwise opens fully
+    client.move_gripper(gripper_width) # moves to narrow if narrow, otherwise opens fully
     time.sleep(4)
     client.move(np.array([x_final, y_final, clearance_height+height, 0, 1.57, 0]))
     time.sleep(4)
@@ -92,35 +56,36 @@ def main():
     client = WidowXClient(host=args.ip, port=args.port)
     client.init(WidowXConfigs.DefaultEnvParams, image_size=256)
 
-    print_help()
     is_open = 1
 
-    client.move(np.array([0.3, 0, 0.15, 0, 1.57, 0]))
-    time.sleep(5)
+    pick_and_place([0.17, -0.1], [0.17, -0.02], 0.04, 0.08, client, gripper_width=0.7)
+
+    # client.move(np.array([0.3, 0, 0.15, 0, 1.57, 0]))
+    # time.sleep(5)
+    # # client.move_gripper(1.0)
+    # # time.sleep(2)
+    # # client.move_gripper(0.5)
+    # # time.sleep(3)
+    # # client.move_gripper(0.2)
+    # # time.sleep(2)
+    # # client.move_gripper(0.0)
+    # client.step_action(np.array([-0.06,0,0,0,0,0,is_open]))
+    # time.sleep(5)
+    # client.step_action(np.array([0,0,-0.05,0,0,0,is_open]))
+    # time.sleep(5)
+    # is_open = 0.1
+    # client.move_gripper(0.1)
+    # time.sleep(5)
+    # client.step_action(np.array([0,0,0.15,0,0,0,is_open]))
+    # time.sleep(5)
+    # client.step_action(np.array([0.1,0.1,0,0,0,0,is_open]))
+    # time.sleep(5)
+    # client.step_action(np.array([0,0,-0.15,0,0,0,is_open]))
+    # time.sleep(5)
     # client.move_gripper(1.0)
-    # time.sleep(2)
-    # client.move_gripper(0.5)
-    # time.sleep(3)
-    # client.move_gripper(0.2)
-    # time.sleep(2)
-    # client.move_gripper(0.0)
-    client.step_action(np.array([-0.06,0,0,0,0,0,is_open]))
-    time.sleep(5)
-    client.step_action(np.array([0,0,-0.05,0,0,0,is_open]))
-    time.sleep(5)
-    is_open = 0.1
-    client.move_gripper(0.1)
-    time.sleep(5)
-    client.step_action(np.array([0,0,0.15,0,0,0,is_open]))
-    time.sleep(5)
-    client.step_action(np.array([0.1,0.1,0,0,0,0,is_open]))
-    time.sleep(5)
-    client.step_action(np.array([0,0,-0.15,0,0,0,is_open]))
-    time.sleep(5)
-    client.move_gripper(1.0)
-    is_open = 1
-    time.sleep(5)
-    client.step_action(np.array([0,0,0.15,0,0,0,is_open]))
+    # is_open = 1
+    # time.sleep(5)
+    # client.step_action(np.array([0,0,0.15,0,0,0,is_open]))
    
 
 
