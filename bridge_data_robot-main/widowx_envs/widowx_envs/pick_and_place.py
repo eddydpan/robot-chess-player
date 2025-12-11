13844dbc82750b40bef89d677ca37293b2bf7b75
eddydpan <eddydotpan@gmail.com>
2025-12-11 01:44:26 -0500

    Begin implementation of PnP

--- bridge_data_robot-main/widowx_envs/widowx_envs/pick_and_place.py
+++ bridge_data_robot-main/widowx_envs/widowx_envs/pick_and_place.py
@@ -40,8 +40,46 @@ def print_help():
     print_yellow("    r: reset robot")
     print_yellow("    q: quit")
 
+def pick_and_place(xy_initial, xy_final, height, clearance_height, client):
+    '''
+    Pick up an object at a certain position  and place it at a different position.
+
+    Args: 
+        xy_initial: 2-item list containing xy position of object in robot coordinate frame
+        xy_final: 2-item list containing xy position of destination in robot coordinate frame
+        height: height of object
+        clearance_height: height the object must be raised to ensure it doesn't hit any other objects
+        client: the client used to control the robot
+    '''
+    x_initial = xy_initial[0]
+    y_initial = xy_initial[1]
+    x_final = xy_final[0]
+    y_final = xy_initial[1]
+
+    client.move(np.array([0.3, 0, 0.15, 0, 1.57, 0])) # Move home
+    time.sleep(4)
+    client.move(np.array([x_initial, y_initial, clearance_height+height, 0, 1.57, 0]))
+    time.sleep(4)
+    client.move(np.array([x_initial, y_initial, height, 0, 1.57, 0]))
+    time.sleep(4)
+    client.move_gripper(0.0)
+    time.sleep(4)
+    client.move(np.array([x_initial, y_initial, clearance_height+height, 0, 1.57, 0]))
+    time.sleep(4)
+    client.move(np.array([x_final, y_final, clearance_height+height, 0, 1.57, 0]))
+    time.sleep(4)
+    client.move(np.array([x_final, y_final, height, 0, 1.57, 0]))
+    time.sleep(4)
+    client.move_gripper(1.0)
+    time.sleep(4)
+    client.move(np.array([x_final, y_final, clearance_height+height, 0, 1.57, 0]))
+    time.sleep(4)
+
+
+    
+
 def main():
-    parser = argparse.ArgumentParser(description='Teleoperation for WidowX Robot')
+    parser = argparse.ArgumentParser(description='Pick and Place for WidowX Robot')
     parser.add_argument('--ip', type=str, default='localhost')
     parser.add_argument('--port', type=int, default=5556)
     args = parser.parse_args()
@@ -50,88 +88,30 @@ def main():
     client.init(WidowXConfigs.DefaultEnvParams, image_size=256)
 
     print_help()
-    cv2.namedWindow("Robot Camera")
     is_open = 1
-    running = True
 
     client.move(np.array([0.3, 0, 0.15, 0, 1.57, 0]))
-    time.sleep(3)
+    time.sleep(5)
     client.step_action(np.array([-0.06,0,0,0,0,0,is_open]))
-    time.sleep(3)
+    time.sleep(5)
     client.step_action(np.array([0,0,-0.05,0,0,0,is_open]))
-    time.sleep(3)
+    time.sleep(5)
     is_open = 0
     client.move_gripper(0.0)
-    time.sleep(3)
+    time.sleep(5)
     client.step_action(np.array([0,0,0.15,0,0,0,is_open]))
-    time.sleep(3)
+    time.sleep(5)
     client.step_action(np.array([0.1,0.1,0,0,0,0,is_open]))
-    time.sleep(3)
+    time.sleep(5)
     client.step_action(np.array([0,0,-0.15,0,0,0,is_open]))
-    time.sleep(3)
+    time.sleep(5)
     client.move_gripper(1.0)
     is_open = 1
-    time.sleep(3)
+    time.sleep(5)
     client.step_action(np.array([0,0,0.15,0,0,0,is_open]))
 
-
-    # Run the asynchronous function
-        
-    # time.sleep(1)
-    # while running:
-    #     # Check for key press
-    #     key = cv2.waitKey(100) & 0xFF
-
-    #     # escape key to quit
-    #     if key == ord('q'):
-    #         print("Quitting teleoperation.")
-    #         running = False
-    #         continue
-
-    #     # Handle key press for robot control
-    #     # translation
-    #     if key == ord('w'):
-    #         client.step_action(np.array([0.01, 0, 0, 0, 0, 0, is_open]))
-    #     elif key == ord('s'):
-    #         client.step_action(np.array([-0.01, 0, 0, 0, 0, 0, is_open]))
-    #     elif key == ord('a'):
-    #         client.step_action(np.array([0, 0.01, 0, 0, 0, 0, is_open]))
-    #     elif key == ord('d'):
-    #         client.step_action(np.array([0, -0.01, 0, 0, 0, 0, is_open]))
-    #     elif key == ord('z'):
-    #         client.step_action(np.array([0, 0, 0.01, 0, 0, 0, is_open]))
-    #     elif key == ord('c'):
-    #         client.step_action(np.array([0, 0, -0.01, 0, 0, 0, is_open]))
-        
-    #     # rotation
-    #     elif key == ord('i'):
-    #         client.step_action(np.array([0, 0, 0, 0.01, 0, 0, is_open]))
-    #     elif key == ord('k'):
-    #         client.step_action(np.array([0, 0, 0, -0.01, 0, 0, is_open]))
-    #     elif key == ord('j'):
-    #         client.step_action(np.array([0, 0, 0, 0, 0.01, 0, is_open]))
-    #     elif key == ord('l'):
-    #         client.step_action(np.array([0, 0, 0, 0, -0.01, 0, is_open]))
-    #     elif key == ord('n'):
-    #         client.step_action(np.array([0, 0, 0, 0, 0, 0.01, is_open]))
-    #     elif key == ord('m'):
-    #         client.step_action(np.array([0, 0, 0, 0, 0, -0.01, is_open]))    
-        
-    #     # space bar to change gripper state
-    #     elif key == ord(' '):
-    #         is_open = 1 - is_open
-    #         print("Gripper is now: ", is_open)
-    #         client.step_action(np.array([0, 0, 0, 0, 0, 0, is_open]))
-    #     elif key == ord('r'):
-    #         print("Resetting robot...")
-    #         client.reset()
-    #         print_help()
-
-    #     show_video(client)
-
     client.stop()  # Properly stop the client
-    cv2.destroyAllWindows()
-    print("Teleoperation ended.")
+    print("Pick and place complete.")
 
 if __name__ == "__main__":
     main()
